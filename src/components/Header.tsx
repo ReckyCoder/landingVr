13ff8d172ac0882478d2aaedf91fba47b7b0bcf1
Alex Leiva <aleiva.vrgroup@bicevida.cl>
2025-07-21 09:38:08 -0400

    ajustes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,31 +1,33 @@
-import Hamburguer from '@/utils/Hamburguer';
-import logoVr from '@img/logo-vr-group.png';
-import { useEffect, useRef, useState, type RefObject } from 'react';
-import texto from '@json/home.json';
-import { NavLink, useLocation } from '@node_modules/react-router/dist/development';
+import Hamburguer from "@/utils/Hamburguer";
+import logoVr from "@img/logo-vr-group.png";
+import { useEffect, useRef, useState, type RefObject } from "react";
+import texto from "@json/home.json";
+import {
+  NavLink,
+  useLocation,
+} from "@node_modules/react-router/dist/development";
 
 type HeaderProps = {
   tipoHeader?: number;
 };
 export default function Header() {
+  const localization = useLocation();
 
-    const localization = useLocation();
+  const [isSticky, setIsSticky] = useState(false);
+  const headerRef = useRef<HTMLElement>(null);
 
-    const [isSticky, setIsSticky] = useState(false);
-    const headerRef = useRef<HTMLElement>(null);
-
-    const [isActivateHamburgerState, setHamburguer] = useState(false);
-    const [isVisibleHamburguerState, setisVisibleHamburguer] = useState(false)
+  const [isActivateHamburgerState, setHamburguer] = useState(false);
+  const [isVisibleHamburguerState, setisVisibleHamburguer] = useState(false);
 
   useEffect(() => {
     const handleResize = () => setisVisibleHamburguer(window.innerWidth < 768);
     const handleScroll = () => setIsSticky(window.scrollY > 120);
 
-        handleResize();
+    handleResize();
 
-         // Llamada inicial
-        window.addEventListener('scroll', handleScroll);
-        window.addEventListener('resize', handleResize);
+    // Llamada inicial
+    window.addEventListener("scroll", handleScroll);
+    window.addEventListener("resize", handleResize);
 
     return () => {
       window.removeEventListener("scroll", handleScroll);
@@ -45,46 +47,82 @@ export default function Header() {
     }
   };
 
-    const styleHeader = " w-full grid grid-cols-3 items-center justify-between colorBg h-[70px] transition-all duration-500 z-3 border border-b-[#474747]"
-    const isHome = (
-        localization.pathname === '/home' && isSticky ? "fixed top-0" : 
-        localization.pathname === '/home' && !isSticky ? "fixed -top-80" : 
-        localization.pathname !== '/home' && !isSticky ? "relative" :
-        localization.pathname !== '/home' && isSticky ? 'fixed top-0' : '')
+  const styleHeader =
+    " w-full grid grid-cols-3 items-center justify-between colorBg h-[70px] transition-all duration-500 z-3 border border-b-[#474747]";
+  const isHome =
+    localization.pathname === "/home" && isSticky
+      ? "fixed top-0"
+      : localization.pathname === "/home" && !isSticky
+      ? "fixed -top-80"
+      : localization.pathname !== "/home" && !isSticky
+      ? "relative"
+      : localization.pathname !== "/home" && isSticky
+      ? "fixed top-0"
+      : "";
 
-    return (
-        <header ref={headerRef} className={isHome + styleHeader}>
-            <div className='flex justify-center'>
-                <img className='w-[130px]' src={logoVr} alt="logo vr" />
-            </div>
-            <nav className='invisible md:visible flex list-none gap-4 text-white justify-center h-full justify-center'>
-                <li className='h-full'>
-                    <NavLink to={"/home"} 
-                        className={({isActive}) => `${isActive ? 'text-(--color-primary-red) border-b-(--color-primary-red) border-b-2' : ""}  items-center flex h-full hover:text-(--color-primary-red)`}>
-                        {texto.inicio}
-                    </NavLink>
-                </li>
-                <li className='h-full'>
-                    <NavLink to={"/nosotros"} 
-                        className={({isActive}) => `${isActive ? 'text-(--color-primary-red) border-b-(--color-primary-red) border-b-2' : ""}  items-center flex h-full hover:text-(--color-primary-red)`}>
-                        {texto.nosotros}
-                    </NavLink>
-                </li>
-                <li className='h-full'>
-                    <NavLink to={"/servicios"} 
-                        className={({isActive}) => `${isActive ? 'text-(--color-primary-red) border-b-(--color-primary-red) border-b-2' : ""}  items-center flex h-full hover:text-(--color-primary-red)`}>
-                        {texto.servicios}
-                    </NavLink>
-                </li>
-            </nav>
-            <div className={isVisibleHamburguerState ? 'flex justify-end' : 'flex justify-center h-full'}>
-                <Hamburguer 
-                    isActivateHamburgerState={isActivateHamburgerState}
-                    isVisibleHamburguerState={isVisibleHamburguerState}
-                    changeHamburguer={changeHamburguer}
-                    headerRef={headerRef}
-                />
-            </div>
-        </header>
-    )
+  return (
+    <header ref={headerRef} className={isHome + styleHeader}>
+      <div className="flex justify-center">
+        <img className="w-[130px]" src={logoVr} alt="logo vr" />
+      </div>
+      <nav className="invisible md:visible flex list-none gap-4 text-white justify-center h-full justify-center">
+        <li className="h-full">
+          <NavLink
+            to={"/home"}
+            className={({ isActive }) =>
+              `${
+                isActive
+                  ? "text-(--color-primary-red) border-b-(--color-primary-red) border-b-2"
+                  : ""
+              }  items-center flex h-full hover:text-(--color-primary-red)`
+            }
+          >
+            {texto.inicio}
+          </NavLink>
+        </li>
+        <li className="h-full">
+          <NavLink
+            to={"/nosotros"}
+            className={({ isActive }) =>
+              `${
+                isActive
+                  ? "text-(--color-primary-red) border-b-(--color-primary-red) border-b-2"
+                  : ""
+              }  items-center flex h-full hover:text-(--color-primary-red)`
+            }
+          >
+            {texto.nosotros}
+          </NavLink>
+        </li>
+        <li className="h-full">
+          <NavLink
+            to={"/servicios"}
+            className={({ isActive }) =>
+              `${
+                isActive
+                  ? "text-(--color-primary-red) border-b-(--color-primary-red) border-b-2"
+                  : ""
+              }  items-center flex h-full hover:text-(--color-primary-red)`
+            }
+          >
+            {texto.servicios}
+          </NavLink>
+        </li>
+      </nav>
+      <div
+        className={
+          isVisibleHamburguerState
+            ? "flex justify-end"
+            : "flex justify-center h-full"
+        }
+      >
+        <Hamburguer
+          isActivateHamburgerState={isActivateHamburgerState}
+          isVisibleHamburguerState={isVisibleHamburguerState}
+          changeHamburguer={changeHamburguer}
+          headerRef={headerRef}
+        />
+      </div>
+    </header>
+  );
 }
--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,82 +1,105 @@
-import logoVr from '@img/logo-vr-group.png';
-import gradient from '@img/gradient.png';
-import bgHome from '@img/bg-home.png';
-import conocenos from '@img/conócenos.png';
-import linkedin from '@img/linkedin.png';
-import texto from '@json/home.json';
-import Hamburguer from '@/utils/Hamburguer';
-import { useEffect, useRef, useState, type RefObject } from 'react';
-import ServiciosYConsultoria from './components/ServiciosYConsultoria';
-import Soluciones from './components/Soluciones';
-import UltimosProyectos from './components/UltimosProyectos';
-import Testimonios from './components/Testimonios';
-import Contacto from '@/components/Contacto';
+import logoVr from "@img/logo-vr-group.png";
+import gradient from "@img/gradient.png";
+import bgHome from "@img/bg-home.png";
+import conocenos from "@img/conócenos.png";
+import linkedin from "@img/linkedin.png";
+import texto from "@json/home.json";
+import Hamburguer from "@/utils/Hamburguer";
+import { useEffect, useRef, useState, type RefObject } from "react";
+import ServiciosYConsultoria from "./components/ServiciosYConsultoria";
+import Soluciones from "./components/Soluciones";
+import UltimosProyectos from "./components/UltimosProyectos";
+import Testimonios from "./components/Testimonios";
+import Contacto from "@/components/Contacto";
 
 export default function Home() {
+  const [isActivateHamburgerState, setHamburguer] = useState(false);
+  const [isVisibleHamburguerState, setisVisibleHamburguer] = useState(false);
 
-    const [isActivateHamburgerState, setHamburguer] = useState(false);
-    const [isVisibleHamburguerState, setisVisibleHamburguer] = useState(false)
+  const headerRef = useRef<HTMLElement>(null);
 
-    const headerRef = useRef<HTMLElement>(null);
+  useEffect(() => {
+    const handleResize = () => setisVisibleHamburguer(window.innerWidth < 768);
 
-    useEffect(() => {
-         const handleResize = () => setisVisibleHamburguer(window.innerWidth < 768);
+    // Ejecutar inmediatamente al montar
+    handleResize();
 
-        // Ejecutar inmediatamente al montar
-        handleResize();
+    // Escuchar eventos de resize
+    window.addEventListener("resize", handleResize);
 
-        // Escuchar eventos de resize
-        window.addEventListener('resize', handleResize);
+    // Limpiar el event listener cuando el componente se desmonte
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
-        // Limpiar el event listener cuando el componente se desmonte
-        return () => {
-            window.removeEventListener('resize', handleResize);
-        };
-    }, [])
-
-    const changeHamburguer = (isActive: boolean, headerRef: RefObject<HTMLElement> | null, setIsHeader: React.Dispatch<React.SetStateAction<boolean>>) => {
-        setHamburguer(isActive);
-        console.log(headerRef?.current.tagName);
-        if(headerRef?.current.tagName !== 'HEADER'){
-            setIsHeader(false);
-        }
+  const changeHamburguer = (
+    isActive: boolean,
+    headerRef: RefObject<HTMLElement> | null,
+    setIsHeader: React.Dispatch<React.SetStateAction<boolean>>
+  ) => {
+    setHamburguer(isActive);
+    console.log(headerRef?.current.tagName);
+    if (headerRef?.current.tagName !== "HEADER") {
+      setIsHeader(false);
     }
+  };
 
+  return (
+    <>
+      <section className="relative flex flex-col">
+        <div className="absolute flex flex-col items-center w-full z-2">
+          <div ref={headerRef} className="absolute right-0 pr-10 top-15">
+            <Hamburguer
+              isActivateHamburgerState={isActivateHamburgerState}
+              isVisibleHamburguerState={isVisibleHamburguerState}
+              changeHamburguer={changeHamburguer}
+              headerRef={headerRef}
+            />
+          </div>
+          <img
+            className="w-[81px] md:w-[155px] transition-all duration-500"
+            src={gradient}
+            alt=""
+          />
+          <img
+            className="top-30 w-[160px] md:w-[308px] md:top-60 absolute transition-all duration-500"
+            src={logoVr}
+            alt=""
+          />
+          <div className="max-w-[560px] text-white">
+            <p className="mt-10 text-center leading-7">{texto.bienvenida}</p>
+          </div>
+          <img
+            className="mt-20 motion-safe:animate-bounce"
+            src={conocenos}
+            alt=""
+          />
+        </div>
+        <img className="object-cover h-[900px] w-full" src={bgHome} alt="" />
+        <div
+          className={
+            isVisibleHamburguerState
+              ? "invisible"
+              : "absolute z-2 bottom-20 left-20 flex flex-col items-center gap-10"
+          }
+        >
+          <a
+            href="https://www.linkedin.com/company/vr-group-chile/"
+            target="_blank"
+          >
+            <img className="w-[25px] h-[25px]" src={linkedin} alt="" />
+          </a>
+          <p className="text-white -rotate-90">{texto.siguenos}</p>
+        </div>
+        <div className="bg-linear-to-b from-black/80 to-black/80 absolute h-full w-full"></div>
+      </section>
 
-    return (
-        <>
-            <section className='relative flex flex-col'>
-                <div className='absolute flex flex-col items-center w-full z-2'>
-                    <div ref={headerRef} className='absolute right-0 pr-10 top-15'>
-                        <Hamburguer 
-                            isActivateHamburgerState={isActivateHamburgerState}
-                            isVisibleHamburguerState={isVisibleHamburguerState}
-                            changeHamburguer={changeHamburguer}
-                            headerRef={headerRef}
-                        />
-                    </div>
-                    <img className='w-[81px] md:w-[155px] transition-all duration-500' src={gradient} alt="" />
-                    <img className='top-30 w-[160px] md:w-[308px] md:top-60 absolute transition-all duration-500' src={logoVr} alt="" />
-                    <div className='max-w-[560px] text-white'>
-                        <p className='mt-10 text-center leading-7'>{texto.bienvenida}</p>
-                    </div>
-                    <img className='mt-20 motion-safe:animate-bounce' src={conocenos} alt="" />
-                </div>
-                <img className='object-cover h-[900px] w-full' src={bgHome} alt="" />
-                <div className={isVisibleHamburguerState ? 'invisible' : "absolute z-2 bottom-20 left-20 flex flex-col items-center gap-10"}>
-                    <a href="https://www.linkedin.com/company/vr-group-chile/" target='_blank'>
-                        <img className='w-[25px] h-[25px]' src={linkedin} alt="" />
-                    </a>
-                    <p className='text-white -rotate-90'>{texto.siguenos}</p>
-                </div>
-                <div className='bg-linear-to-b from-black/80 to-black/80 absolute h-full w-full'></div>
-            </section>
-
-            <ServiciosYConsultoria/>
-            <Soluciones />
-            <UltimosProyectos/>
-            <Testimonios/>
-            <Contacto/>
-        </>
-    )
+      <ServiciosYConsultoria />
+      <Soluciones />
+      <UltimosProyectos />
+      <Testimonios />
+      <Contacto />
+    </>
+  );
 }
