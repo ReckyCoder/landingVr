d155951a81a1c1d303b5e77d12a087afd525c597
Alex Leiva <aleiva.vrgroup@bicevida.cl>
2025-07-21 05:08:02 -0400

    ajustes

--- src/pages/home/components/Soluciones.tsx
+++ src/pages/home/components/Soluciones.tsx
@@ -1,92 +1,105 @@
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Pagination } from 'swiper/modules';
-import { useState } from 'react';
-import texto from '@json/home.json';
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination } from "swiper/modules";
+import { useState } from "react";
+import texto from "@json/home.json";
 import "@node_modules/swiper/swiper.css";
-import 'swiper/css/pagination';
-import computerAndMobileIMG from '@img/computerAndMobile.png';
-import neuronIMG from '@img/neuron.png';
-import circleFragmentIMG from '@img/circleFragment.png';
-import CardSimple from '@/components/CardSimple';
-import type { ActividadItem } from '@/pages/serviciosDetalle/ServiciosDetalle';
-
+import "swiper/css/pagination";
+import computerAndMobileIMG from "@img/computerAndMobile.png";
+import neuronIMG from "@img/neuron.png";
+import circleFragmentIMG from "@img/circleFragment.png";
+import CardSimple from "@/components/CardSimple";
+import type { ActividadItem } from "@/pages/serviciosDetalle/ServiciosDetalle";
 
 export default function Soluciones() {
+  const [tipoContenido, setTipoContenido] = useState(1);
 
-    const [tipoContenido, setTipoContenido] = useState(1);
+  const soluciones: ActividadItem[] = [
+    {
+      icono: computerAndMobileIMG,
+      titulo: texto.experienciaDigital,
+      descripcion: texto.lorem,
+    },
+    {
+      icono: neuronIMG,
+      titulo: texto.automatización,
+      descripcion: texto.lorem,
+    },
+    {
+      icono: circleFragmentIMG,
+      titulo: texto.solucionesTecnologicas,
+      descripcion: texto.lorem,
+    },
+    {
+      icono: computerAndMobileIMG,
+      titulo: texto.experienciaDigital,
+      descripcion: texto.lorem,
+    },
+  ];
 
-    const soluciones : ActividadItem[] = [
+  const changeCategory = (id: number) => {
+    setTipoContenido(id);
+  };
 
-        {
-            icono: computerAndMobileIMG,
-            titulo: texto.experienciaDigital,
-            descripcion: texto.lorem
-        },
-        {
-            icono: neuronIMG,
-            titulo: texto.automatización,
-            descripcion: texto.lorem
-        },
-        {
-            icono: circleFragmentIMG,
-            titulo: texto.solucionesTecnologicas,
-            descripcion: texto.lorem
-        },
-        {
-            icono: computerAndMobileIMG,
-            titulo: texto.experienciaDigital,
-            descripcion: texto.lorem
-        },
-    ]
+  const styleCategory = "font-bold border-b-1 hover:cursor-pointer text-[14px]";
 
-    const changeCategory = (id: number) => {
-        setTipoContenido(id)
-    }
+  return (
+    <section className="flex flex-col items-center justify-center max-w-[1400px] mt-10 mx-auto pt-[80px] px-[5%]">
+      <div className="max-w-[350px] text-center">
+        <p className="text-[32px] font-bold flex flex-wrap justify-center gap-x-2">
+          <span>{texto.nuestras}</span>
+          <span className="colorRed">{texto.solucionesNegocio}</span>
+          <span>{texto.negocio}</span>
+        </p>
+      </div>
 
-    const styleCategory = "font-bold border-b-1 hover:cursor-pointer text-[14px]";
+      <div className="my-10 flex gap-x-10">
+        <button
+          className={
+            tipoContenido === 1
+              ? styleCategory + " colorRed"
+              : styleCategory +
+                " border-b-[#e9e9e9] hover:border-b-[#ccc] transition-colors duration-200"
+          }
+          onClick={() => changeCategory(1)}
+        >
+          {texto.transformación}
+        </button>
+        <button
+          className={
+            tipoContenido === 2
+              ? styleCategory + " colorRed"
+              : styleCategory +
+                " border-b-[#e9e9e9] hover:border-b-[#ccc] transition-colors duration-200"
+          }
+          onClick={() => changeCategory(2)}
+        >
+          {texto.gestión}
+        </button>
+      </div>
 
-    return (
-        <section className='flex flex-col items-center justify-center max-w-[1400px] mt-10 mx-auto pt-[80px] px-[5%]'>
-            <div className='max-w-[350px] text-center'>
-                <p className='text-[32px] font-bold flex flex-wrap justify-center gap-x-2'>
-                    <span>{texto.nuestras}</span> 
-                    <span className='colorRed'>{texto.solucionesNegocio}</span> 
-                    <span>{texto.negocio}</span>
-                </p>
-            </div>
-
-            <div className='my-10 flex gap-x-10'>
-                <button className={tipoContenido === 1 ? 
-                    styleCategory + ' colorRed' : 
-                    styleCategory + ' border-b-[#e9e9e9] hover:border-b-[#ccc] transition-colors duration-200'
-                } onClick={() => changeCategory(1)}>{texto.transformación}</button>
-                <button className={tipoContenido === 2 ? 
-                    styleCategory + ' colorRed' : 
-                    styleCategory + ' border-b-[#e9e9e9] hover:border-b-[#ccc] transition-colors duration-200'
-                } onClick={() => changeCategory(2)}>{texto.gestión}</button>
-            </div>
-                
-            {tipoContenido === 1 ? 
-                (
-                    <CardSimple
-                        arrayActividades={soluciones}
-                        sliderPerViewDesktop={3}
-                        sliderPerViewMobile={1}
-                    >
-                        <a href='#' className='colorRed text-[14px] mt-2 font-bold'>{texto.saberMás}</a>
-                    </CardSimple>
-                )
-                :
-                (
-                    <CardSimple
-                        arrayActividades={soluciones}
-                        sliderPerViewDesktop={3}
-                        sliderPerViewMobile={1}
-                    >
-                        <a href='#' className='colorRed text-[14px] mt-2 font-bold'>{texto.saberMás}</a>
-                    </CardSimple>
-                )
-            }
-        </section>
-    )
+      {tipoContenido === 1 ? (
+        <CardSimple
+          arrayActividades={soluciones}
+          sliderPerViewDesktop={3}
+          sliderPerViewMobile={1}
+          autoPlay={6000}
+        >
+          <a href="#" className="colorRed text-[14px] mt-2 font-bold">
+            {texto.saberMás}
+          </a>
+        </CardSimple>
+      ) : (
+        <CardSimple
+          arrayActividades={soluciones}
+          sliderPerViewDesktop={3}
+          sliderPerViewMobile={1}
+          autoPlay={6000}
+        >
+          <a href="#" className="colorRed text-[14px] mt-2 font-bold">
+            {texto.saberMás}
+          </a>
+        </CardSimple>
+      )}
+    </section>
+  );
 }
--- src/pages/home/components/UltimosProyectos.tsx
+++ src/pages/home/components/UltimosProyectos.tsx
@@ -1,90 +1,108 @@
-import texto from '@json/home.json';
-import rectangle from '@img/rectangle.png';
-import project1 from '@img/project1.png';
+import texto from "@json/home.json";
+import rectangle from "@img/rectangle.png";
+import project1 from "@img/project1.png";
 import { Swiper, SwiperSlide } from "swiper/react";
-import { Pagination } from "swiper/modules";
+import { Autoplay, Navigation, Pagination } from "swiper/modules";
 
 export default function UltimosProyectos() {
+  const dataProyectos = [
+    {
+      proyecto: {
+        titulo: texto.casosVR,
+        subTitulo: texto.puertoDeIdeas,
+        descripcion: texto.proyectoExperiencia,
+      },
+      img: project1,
+      contenido: {
+        titulo: texto.loremShort,
+        subTitulo: texto.lorem,
+        link: texto.verDetalle,
+      },
+    },
+    {
+      proyecto: {
+        titulo: texto.casosVR,
+        subTitulo: texto.puertoDeIdeas,
+        descripcion: texto.proyectoExperiencia,
+      },
+      img: project1,
+      contenido: {
+        titulo: texto.loremShort,
+        subTitulo: texto.lorem,
+        link: texto.verDetalle,
+      },
+    },
+    {
+      proyecto: {
+        titulo: texto.casosVR,
+        subTitulo: texto.puertoDeIdeas,
+        descripcion: texto.proyectoExperiencia,
+      },
+      img: project1,
+      contenido: {
+        titulo: texto.loremShort,
+        subTitulo: texto.lorem,
+        link: texto.verDetalle,
+      },
+    },
+  ];
 
+  return (
+    <section className="flex flex-col items-center relative mb-20 2xl:mb-0 px-[5%]">
+      <h2 className="font-bold text-[32px] my-20 text-center">
+        {texto.nuestros} <span className="colorRed">{texto.últimos}</span>{" "}
+        {texto.proyectos}
+      </h2>
 
-    const dataProyectos = [
-        {
-            proyecto: {
-                titulo: texto.casosVR,
-                subTitulo: texto.puertoDeIdeas,
-                descripcion: texto.proyectoExperiencia
-            },
-            img: project1,
-            contenido: {
-                titulo: texto.loremShort,
-                subTitulo: texto.lorem,
-                link: texto.verDetalle
-            }
-        },
-        {
-            proyecto: {
-                titulo: texto.casosVR,
-                subTitulo: texto.puertoDeIdeas,
-                descripcion: texto.proyectoExperiencia
-            },
-            img: project1,
-            contenido: {
-                titulo: texto.loremShort,
-                subTitulo: texto.lorem,
-                link: texto.verDetalle
-            }
-        },
-        {
-            proyecto: {
-                titulo: texto.casosVR,
-                subTitulo: texto.puertoDeIdeas,
-                descripcion: texto.proyectoExperiencia
-            },
-            img: project1,
-            contenido: {
-                titulo: texto.loremShort,
-                subTitulo: texto.lorem,
-                link: texto.verDetalle
-            }
-        }
-    ]
-
-    return (
-        <section className='flex flex-col items-center relative mb-20 2xl:mb-0 px-[5%]'>
-            <h2 className='font-bold text-[32px] my-20 text-center'>{texto.nuestros} <span className='colorRed'>{texto.últimos}</span> {texto.proyectos}</h2>
-
-
-            <div className='w-full swiper-projects'>
-                <Swiper
-                    modules={[Pagination]}
-                    slidesPerView={1}
-                    pagination={{ 
-                        clickable: true,
-                    }}
-                >
-                    {dataProyectos.map((data) => (
-
-                    <SwiperSlide>
-                        <div className='max-w-[600px] mx-auto 2xl:max-w-full grid grid-cols-2 justify-center 2xl:justify-between 2xl:grid-cols-3 items-center z-1'>
-                            <div className='max-w-[300px] mx-auto'>
-                                <p className='colorRed font-bold'>{data.proyecto.titulo}</p>
-                                <p className='text-[30px] font-black colorRed uppercase'>{data.proyecto.subTitulo}</p>
-                                <p className='text-[18px] font-bold'>{data.proyecto.descripcion}</p>
-                            </div>
-                            <div className='flex justify-center items-center relative col-span-2 2xl:col-span-1'>
-                                <img className='w-20 2xl:w-[418px] h-[818px] 2xl:h-[721px] absolute right-0 -z-1 2xl:visible 2xl:relative' src={rectangle} alt="" />
-                                <img className='w-[623px] 2xl:absolute' src={data.img} alt="" />
-                            </div>
-                            <div className='max-w-[300px] mx-auto flex flex-col gap-y-6'>
-                                <p className='font-bold'>{data.contenido.titulo}</p>
-                                <p className='font-bold text-[14px] text-[#939393]'>{data.contenido.subTitulo}</p>
-                                <a href='#' className='font-bold text-[14px] bg-primary-red text-white rounded-[20px] py-2 px-5 w-fit cursor-pointer'>{data.contenido.link}</a>
-                            </div>
-                        </div>
-                    </SwiperSlide>
-                    ))}
-                </Swiper>
-            </div>
-        </section>
-    )
+      <div className="w-full swiper-projects">
+        <Swiper
+          modules={[Navigation, Pagination, Autoplay]}
+          slidesPerView={1}
+          loop={true}
+          pagination={{ clickable: true }}
+          autoplay={{ delay: 5000 }}
+        >
+          {dataProyectos.map((data) => (
+            <SwiperSlide>
+              <div className="max-w-[600px] mx-auto 2xl:max-w-full grid grid-cols-2 justify-center 2xl:justify-between 2xl:grid-cols-3 items-center z-1">
+                <div className="max-w-[300px] mx-auto">
+                  <p className="colorRed font-bold">{data.proyecto.titulo}</p>
+                  <p className="text-[30px] font-black colorRed uppercase">
+                    {data.proyecto.subTitulo}
+                  </p>
+                  <p className="text-[18px] font-bold">
+                    {data.proyecto.descripcion}
+                  </p>
+                </div>
+                <div className="flex justify-center items-center relative col-span-2 2xl:col-span-1">
+                  <img
+                    className="w-20 2xl:w-[418px] h-[818px] 2xl:h-[721px] absolute right-0 -z-1 2xl:visible 2xl:relative"
+                    src={rectangle}
+                    alt=""
+                  />
+                  <img
+                    className="w-[623px] 2xl:absolute"
+                    src={data.img}
+                    alt=""
+                  />
+                </div>
+                <div className="max-w-[300px] mx-auto flex flex-col gap-y-6">
+                  <p className="font-bold">{data.contenido.titulo}</p>
+                  <p className="font-bold text-[14px] text-[#939393]">
+                    {data.contenido.subTitulo}
+                  </p>
+                  <a
+                    href="#"
+                    className="font-bold text-[14px] bg-primary-red text-white rounded-[20px] py-2 px-5 w-fit cursor-pointer"
+                  >
+                    {data.contenido.link}
+                  </a>
+                </div>
+              </div>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </section>
+  );
 }
